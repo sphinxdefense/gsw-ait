81e8998a7a2916c5af920c3a273dcc598377a53b
Cameron Jackson <cameron@sphinxdefense.com>
2024-08-30 09:14:09 -0700

    add debug command and command endpoint

--- gswait/handlers/debug_hanlder.py
+++ gswait/handlers/debug_hanlder.py
@@ -16,7 +16,9 @@ class DebugHandler(Handler):
         new_time = time.time()
         ait.core.log.info(f"Packet rx delta: {new_time - self.time}")
         self.time = new_time
-        ait.core.log.info(f"Debug handler received {len(input_data)} bytes")
+        ait.core.log.info(
+            f"Debug handler received {len(input_data)} bytes: {input_data}"
+        )
         # ait.core.log.info(input_data)
         # self.data.append(input_data)
         # if len(self.data) > 1000 and not self.file_write_done:
