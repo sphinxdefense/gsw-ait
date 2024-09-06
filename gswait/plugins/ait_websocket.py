77af4b90e521368ad5ce92ab4c7e04d648563c40
Cameron Jackson <cameron@sphinxdefense.com>
2024-09-06 11:44:41 -0700

    remove filter

--- gswait/plugins/ait_websocket.py
+++ gswait/plugins/ait_websocket.py
@@ -968,7 +968,7 @@ def handle_tlm_realtime():
                 try:
                     name, delta, dntoeus, counter = session.deltas.popleft(timeout=30)
                     uid, packet, _ = session.telemetry.popleft(timeout=30)
-                    if packet and uid in [65]:
+                    if packet:
                         wsock.send(
                             json.dumps(
                                 {
